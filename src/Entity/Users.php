4b6cb18d15a37256b95f813242a4ae4246461fa9
AntoineJuchault <juchaultantoine@protonmail.com>
2024-10-31 20:31:21 +0100

    noice

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -61,7 +61,8 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function __construct()
     {
-        $this->orders = new ArrayCollection();
+		$this->orders = new ArrayCollection();
+		$this->created_at = new \DateTimeImmutable();
     }
 
     public function getId(): ?int
